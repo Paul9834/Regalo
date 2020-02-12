173094a62b16c120b9f66198ec3beb626fd7d2a3
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-02-12 15:13:04 -0500

    test

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home - Brand</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+    <link rel="stylesheet" href="assets/css/smoothproducts.css">
+</head>
+
+<body>
+    <main class="page landing-page">
+
+        <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-dark" id="mainNav">
+            <div class="container">
+                <a href="#" class="navbar-brand">
+                    <img src="https://i.imgur.com/AJ23rMl.png" width="200" height="60">
+                </a>
+                <button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right" type="button" data-toogle="collapse" aria-controls="navbarResponsive" aria-expanded="false"
+                        aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="nav navbar-nav ml-auto text-uppercase">
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <section class="clean-block clean-hero" style="background-image: url(&quot;&quot;)">
+            <div class="text" style="color: z;">
+                <h4>¡Bienvenido al City Campus!</h4><iframe class="d-inline-flex" allowfullscreen="" frameborder="0" src="https://www.youtube.com/embed/VdkSij-p4Ds?autoplay=1&amp;mute=1" width="350" height="200" style="margin: 40px;"></iframe>
+                <p></p><button class="btn btn-outline-light btn-lg" type="button" style="background-color: #e40000;">BOTON1</button><button class="btn btn-outline-light btn-lg" type="button" style="background-color: #ffd600;">BOTON2</button><button class="btn btn-outline-light btn-lg"
+                    type="button" style="background-color: #236eff;">BOTON3</button></div>
+        </section>
+    </main>
+    <footer class="page-footer dark">
+        <div class="footer-copyright">
+            <p>© 2020 Formación por Retos.</p>
+        </div>
+    </footer>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+    <script src="assets/js/smoothproducts.min.js"></script>
+    <script src="assets/js/theme.js"></script>
+</body>
+
+</html>
